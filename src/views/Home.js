46d57760bfd34b765548652361b2e1a01ff5a971
Mario H. Burgos <mario@petrielabs.com>
2018-11-04 22:17:07 +0000

    Adds shelf names from shelf title props.

--- src/views/Home.js
+++ src/views/Home.js
@@ -9,10 +9,10 @@ class Home extends Component {
                 <div className="list-books-title">
                   <h1>MyReads</h1>
                 </div>
-                <div className="list-books-content">
-                    <div>
-                        <BookShelf />
-                    </div>
+                <div className="list-books-content">    
+                    <BookShelf title="Currently Reading"/>
+                    <BookShelf title="Want To Read"/>
+                    <BookShelf title="Read"/>
                     <div>
                         <FindAddBook />
                     </div>
