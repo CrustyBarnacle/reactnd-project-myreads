3d2c9c8e89b271fa6691fd414814339e569ad7d7
Mario H. Burgos <mario@petrielabs.com>
2018-11-04 22:41:36 +0000

    Commented out import of BookShelf and FindAddBook.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import BookShelf from './components/BookShelf';
-import FindAddBook from './components/FindAddBook';
+// import BookShelf from './components/BookShelf';
+// import FindAddBook from './components/FindAddBook';
 import { Switch, Route } from 'react-router-dom'
 import Home from "./views/Home";
 import Search from "./views/Search";
