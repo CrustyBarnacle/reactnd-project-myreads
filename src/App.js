d00f8516acbbe981adfb8eea1eb1c0f9a0cbe2e9
Mario H. Burgos <mario@petrielabs.com>
2018-11-05 00:45:48 +0000

    Removes import and use of Switch.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import BookShelf from './components/BookShelf';
 // import FindAddBook from './components/FindAddBook';
-import { Switch, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Home from "./views/Home";
 import Search from "./views/Search";
 // import * as BooksAPI from './BooksAPI'
@@ -13,10 +13,10 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-          <Switch>
+
               <Route exact path={"/"} component={Home} />
               <Route exact path={"/search"} component={Search} />
-          </Switch>
+
       </div>
     )
   }
