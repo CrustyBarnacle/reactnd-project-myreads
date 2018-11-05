49bcb998809bd8f5ea6e00717d3ab0f7de8327eb
Mario H. Burgos <mario@petrielabs.com>
2018-11-05 09:11:33 +0000

    Adds props (books) to BookShelf component calls.

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,15 +6,24 @@ import BookShelf from '../components/BookShelf';
 
 
 class Home extends Component {
-    async componentDidMount() {
-        try {
-            const books = await BooksAPI.getAll();
-            console.log(books)
-        }
-        catch(error) {
-            console.log(error)
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: []
         }
     }
+    
+componentDidMount() {
+    try {
+        BooksAPI.getAll()
+        .then(response => {
+            this.setState({ books: response});
+        });
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
     render() {
         return (          
             <div className="list-books">
@@ -23,9 +32,9 @@ class Home extends Component {
                 </div>
                 <div className="list-books-content">
                     <div>    
-                        <BookShelf title="Currently Reading"/>
-                        <BookShelf title="Want To Read"/>
-                        <BookShelf title="Read"/>
+                        <BookShelf title="Currently Reading" books={this.state.books.filter(b => b.shelf === "currentlyReading")}/>
+                        <BookShelf title="Want To Read" books={this.state.books.filter(b => b.shelf === "wantToRead")}/>
+                        <BookShelf title="Read" books={this.state.books.filter(b => b.shelf === "read")}/>
                     </div>
                 </div>
                 <div className="open-search">
