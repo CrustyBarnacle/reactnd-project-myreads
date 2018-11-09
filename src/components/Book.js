ecd0a19dce831ab348054da03155ef294d3f27dd
Mario Burgos <mario@petrielabs.com>
2018-11-09 00:34:14 +0000

    Sets default shelf selection value.

--- src/components/Book.js
+++ src/components/Book.js
@@ -14,7 +14,7 @@ class Book extends Component {
 		    backgroundImage: `url("${this.props.book.imageLinks && this.props.book.imageLinks.thumbnail || "" }")` }}>
 	      </div>
               <div className="book-shelf-changer">
-                <select>
+                <select value={this.props.book.shelf}>
                   <option value="move" disabled>Move to...</option>
                   <option value="currentlyReading">Currently Reading</option>
                   <option value="wantToRead">Want to Read</option>
