2b89eb32646220c1f7596ca0fea96ba9b1d407b0
Mario H. Burgos <mario@petrielabs.com>
2018-11-10 20:30:54 +0000

    Adds UpdateBook(), constructor, componentDidMount to search.

--- src/views/Search.js
+++ src/views/Search.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as BooksAPI from '../BooksAPI'
+
 
 class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: [],
+            results: []
+        }
+    }
+    
+    componentDidMount() {
+        try {
+            BooksAPI.getAll()
+            .then(response => {
+                this.setState({ books: response});
+            });
+        }
+        catch(error) {
+            console.log(error)
+        }
+    }    
+    
+    updateBook = (book, shelf) => {
+        BooksAPI.update(book, shelf).then(response => {
+            book.shelf = shelf;
+            this.setState(state => ({
+                books: state.books.filter(b => b.id !== book.id).concat([book])
+            }));
+        });
+    }
+    
     render() {
         return (
             <div className="search-books">
